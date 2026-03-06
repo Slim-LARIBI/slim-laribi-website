c13aac0755472b34e88eece0378f2270ee1d856f
Slim-LARIBI <laribi.slim@gmail.com>
2026-03-06 11:59:56 +0100

    Force default locale to fr

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,11 @@
-// middleware.ts
-import createMiddleware from 'next-intl/middleware';
+import createMiddleware from 'next-intl/middleware'
 
 export default createMiddleware({
   locales: ['fr', 'en'],
-  defaultLocale: 'fr'
-});
+  defaultLocale: 'fr',
+  localeDetection: false,
+})
 
 export const config = {
-  // Match all pathnames except for
-  // - /api (API routes)
-  // - /_next (Next.js internals)
-  // - /_vercel (Vercel internals)
-  // - files with an extension (e.g. favicon.ico)
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
-};
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
+}
